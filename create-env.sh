d776485400c6ae0c5f001e4fbf5cb22cdbc126e0
Viveksir <1982vivekmate@gmail.com>
2019-04-17 12:29:05 +0530

    msg1

--- create-env.sh
+++ create-env.sh
@@ -5,10 +5,10 @@ sleep 2;
 for i in `seq $cont_count`
 do
 	echo "=============================="
-    echo "Creating www.gamutkart$i container.."
+    echo "Creating server$i container.."
     sleep 1
-    docker run --name www.gamutkart$i -d -it --rm nageshvkn/gamutkart-img /bin/bash
+    docker run -it --name server$i 1982vivekmate/nginx-img  /bin/bash
     echo "www.gamutkart$i container has been created!"
 	echo "=============================="
 done
-docker inspect --format {{.NetworkSettings.Networks.bridge.IPAddress}} `docker ps -a -q` > IPs.txt
+
