9a880e7e6c1974a396edc085a0f823a3441f02c5
vivekdata <45874921+vivekdata@users.noreply.github.com>
2019-04-21 21:11:19 +0530

    Update create-env.sh

--- create-env.sh
+++ create-env.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 cont_count=$1
 echo "creating $cont_count containers.."
-sleep 3;
+sleep 2;
 for i in `seq $cont_count`
 do
 	echo "=============================="
-    echo "Creating server$i container.."
+    echo "server$i container.."
     sleep 3
-    sudo docker run -it --name server$i 1982vivekmate/nginx-img /bin/bash
+    docker run -it --name SERVER$i 1982vivekmate/nginx-img /bin/bash
     echo "server$i container has been created!"
 	echo "=============================="
 done
-
+docker inspect --format {{.NetworkSettings.Networks.bridge.IPAddress}} `docker ps -a -q` > IPs.txt
