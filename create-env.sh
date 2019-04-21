7784a483b5437befdb0e73f652938050dd5b3fae
vivekdata <45874921+vivekdata@users.noreply.github.com>
2019-04-21 21:00:07 +0530

    Update create-env.sh

--- create-env.sh
+++ create-env.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cont_count=$1
 echo "creating $cont_count containers.."
-sleep 2;
+sleep 3;
 for i in `seq $cont_count`
 do
 	echo "=============================="
     echo "Creating server$i container.."
-    sleep 1
+    sleep 3
     sudo docker run -it --name server$i 1982vivekmate/nginx-img /bin/bash
     echo "server$i container has been created!"
 	echo "=============================="
